3ff75059a2b6aed1333a56b30fc7715cc4a83388
Supermario <supermariobros@nintendo.com>
2022-03-15 20:55:03 +0100

    Versión 2.0 de la interfaz. - hacer que los mensajes suban hacia arriba

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace App1
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,8 +24,16 @@ namespace App1
         private void Button_Clicked(object sender, EventArgs e)
         {
             var m = mensaje.Text.ToString();
-            usuario.Text = m;
 
+
+            string usuario1Antiguo = usuario1.Text.ToString();
+            string usuario2Antiguo = usuario2.Text.ToString();
+            string usuario3Antiguo = usuario3.Text.ToString();
+
+            usuario1.Text = m;
+            usuario2.Text = usuario1Antiguo;
+            usuario3.Text = usuario2Antiguo;
+            usuario4.Text = usuario3Antiguo;
 
             try
             {
@@ -54,15 +63,31 @@ namespace App1
                     }
                  );
               
-                //Intento de convertir json a objeto:
-                
-                //El objeto root que digo es este de aqui abajo vv
+                //Convertir json a objeto:
                 Root deserialized = JsonConvert.DeserializeObject<Root>(result2.Response.ToString());
-                eva.Text = deserialized.output.generic[0].text.ToString();
+                
+                //Desplazar y mostrar mensaje:
+                string eva1Antiguo = eva1.Text.ToString();
+                string eva2Antiguo = eva2.Text.ToString();
+                string eva3Antiguo = eva2.Text.ToString();
 
+                eva1.Text = deserialized.output.generic[0].text.ToString();
+                eva2.Text = eva1Antiguo;
+                eva3.Text = eva2Antiguo;
+                eva4.Text = eva3Antiguo;
+
+                string usuario1Antiguoo = usuario1.Text.ToString();
+                string usuario2Antiguoo = usuario2.Text.ToString();
+                string usuario3Antiguoo = usuario3.Text.ToString();
+
+                usuario1.Text = "";
+                usuario2.Text = usuario1Antiguoo;
+                usuario3.Text = usuario2Antiguoo;
+                usuario4.Text = usuario3Antiguoo;
+
+                mensaje.Text = "";
 
                 //Console.WriteLine(result2.Response);
-                //eva.Text = result2.Response.ToString();
             }
             catch (ServiceResponseException es)
             {
