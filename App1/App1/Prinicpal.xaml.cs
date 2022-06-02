d03b228eb344fadf8dd8f5e8cf05fc9d74fa5f3d
Supermario <supermariobros@nintendo.com>
2022-06-02 19:58:27 +0200

    tool bar de la página detalle dinamica

--- App1/App1/Prinicpal.xaml.cs
+++ App1/App1/Prinicpal.xaml.cs
@@ -1,4 +1,5 @@
-﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+﻿using APIEva.Models;
+using IBM.Cloud.SDK.Core.Authentication.Iam;
 using IBM.Cloud.SDK.Core.Http.Exceptions;
 using IBM.Watson.Assistant.v2;
 using IBM.Watson.Assistant.v2.Model;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -15,19 +17,80 @@ namespace App1
 {
     public partial class Principal : MasterDetailPage
     {
+        HttpClient client;
+        public Color barcolor { get; private set; }
         public Principal(string user)
         {
             InitializeComponent();
+
+            client = new HttpClient();
+            string user_name = user;
+
+            loadDetailPageAsync(user_name);
+
+            loadPage(user_name);
+
+
+
+        }
+        private void loadPage(string user_name){
             //Metemos el menu desplegable lateral
             this.Master = new Master();
+
             //Metemos la pagina principal
-            this.Detail = new NavigationPage(new Detail(user))
+            this.Detail = new NavigationPage(new Detail(user_name))
             {
-                BarBackgroundColor = Color.FromHex("#6656bc"),
+                BarBackgroundColor = barcolor,
                 BarTextColor = Color.White,
             };
+        }
 
-           
+        private async Task<Configuration> getConfigurationsAsync(string user_name)
+        {
+
+            Uri uri = new Uri("https://apieva2022.azurewebsites.net/api/Configuration/" + user_name);
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string content = await response.Content.ReadAsStringAsync();
+                Configuration c = JsonConvert.DeserializeObject<Configuration>(content);
+
+                return c;
+
+            }
+            return null;
+        }
+
+        private async Task loadDetailPageAsync(string user_name)
+        {
+            Configuration c = await getConfigurationsAsync(user_name);
+
+            switch (c.color)
+            {
+                case "purple":
+                    barcolor = Color.FromHex("#6656bc");
+                    break;
+                case "green":
+                    barcolor = Color.FromHex("#bae860");
+                    break;
+                case "white":
+                    barcolor = Color.FromHex("#e5c6db");
+                    break;
+                case "black":
+                    barcolor = Color.FromHex("#30383a");
+                    break;
+                case "red":
+                    barcolor = Color.FromHex("#f43f4f");
+                    break;
+                case "blue":
+                    barcolor = Color.FromHex("#75b2dd");
+                    break;
+            }
+            loadPage(user_name);
+
         }
     }
 }
